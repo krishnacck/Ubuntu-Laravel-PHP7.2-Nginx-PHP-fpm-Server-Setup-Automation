89e324a7c2332a00bdfd855a8b5f766c02d7b9f5
krishnacck <krishnacck@gmail.com>
2018-11-01 11:54:00 +0530

    laravel.sh

---
+++ laravel.sh
@@ -0,0 +1,130 @@
+echo -n "Please Type Your Domain Name=="
+read answer
+if whiptail --yesno "You are installing $answer. Are you sure want to continue?" 20 60 ;then
+    echo Yes
+    sudo apt-get update
+	apt-get install software-properties-common -y 
+	apt-get install python-software-properties -y
+	add-apt-repository ppa:ondrej/php -y
+	apt-get -y install unzip zip nginx php7.2 php7.2-mysql php7.2-fpm php7.2-mbstring php7.2-xml php7.2-curl
+	rm -f /etc/nginx/sites-enabled/default;
+	if whiptail --yesno "You are installing $answer as Default Server. If your answer is yes please select Yes, If you want to install as a secondary domain please select No" 20 60 ;then
+		cat <<EOF > /etc/nginx/sites-available/$answer
+		server {
+		    listen 80 default_server;
+		    listen [::]:80 default_server ipv6only=on;
+			root /home/web/$answer/public;
+			index index.php index.html index.htm;
+			server_name localhost;
+			charset   utf-8;
+			gzip on;
+			    gzip_vary on;
+			    gzip_disable "msie6";
+			    gzip_comp_level 6;
+			    gzip_min_length 1100;
+			    gzip_buffers 16 8k;
+			    gzip_proxied any;
+			    gzip_types
+			        text/plain
+			        text/css
+			        text/js
+			        text/xml
+			        text/javascript
+			        application/javascript
+			        application/x-javascript
+			        application/json
+			        application/xml
+			        application/xml+rss;
+			location / {
+			        try_files \$uri \$uri/ /index.php?\$query_string;
+			    }
+			location ~ \.php\$ {
+			        try_files \$uri /index.php =404;
+			        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+			        fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+			        fastcgi_index index.php;
+			        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+			        include fastcgi_params;
+			    }
+			location ~* \.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc|svg|woff|woff2|ttf)\$ {
+			      expires 1M;
+			      access_log off;
+			      add_header Cache-Control "public";
+			    }
+			location ~* \.(?:css|js)\$ {
+			      expires 7d;
+			      access_log off;
+			      add_header Cache-Control "public";
+			    }
+			location ~ /\.ht {
+			        deny  all;
+			    }
+			}
+EOF
+	else
+		cat <<EOF > /etc/nginx/sites-available/$answer
+		server {
+		    listen 80;
+		    listen [::]:80;
+			root /home/web/$answer/public;
+			index index.php index.html index.htm;
+			server_name $answer;
+			charset   utf-8;
+			gzip on;
+			    gzip_vary on;
+			    gzip_disable "msie6";
+			    gzip_comp_level 6;
+			    gzip_min_length 1100;
+			    gzip_buffers 16 8k;
+			    gzip_proxied any;
+			    gzip_types
+			        text/plain
+			        text/css
+			        text/js
+			        text/xml
+			        text/javascript
+			        application/javascript
+			        application/x-javascript
+			        application/json
+			        application/xml
+			        application/xml+rss;
+			location / {
+			        try_files \$uri \$uri/ /index.php?\$query_string;
+			    }
+			location ~ \.php\$ {
+			        try_files \$uri /index.php =404;
+			        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+			        fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+			        fastcgi_index index.php;
+			        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+			        include fastcgi_params;
+			    }
+			location ~* \.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc|svg|woff|woff2|ttf)\$ {
+			      expires 1M;
+			      access_log off;
+			      add_header Cache-Control "public";
+			    }
+			location ~* \.(?:css|js)\$ {
+			      expires 7d;
+			      access_log off;
+			      add_header Cache-Control "public";
+			    }
+			location ~ /\.ht {
+			        deny  all;
+			    }
+			}
+EOF
+	fi
+	ln -s /etc/nginx/sites-available/$answer /etc/nginx/sites-enabled/$answer
+	apt-get install composer -y
+	mkdir /home/web
+	mkdir /home/web/$answer
+	/etc/init.d/nginx restart
+	composer create-project laravel/laravel /home/web/$answer
+	mv /home/web/$answer/.env.example /home/web/$answer/.env
+	cd /home/web/$answer && php artisan key:generate
+	chown -R www-data:www-data /home/web/$answer
+	chmod -R 775 /home/web/$answer/storage
+else
+    echo No
+fi
